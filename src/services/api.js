bf25753a1267a84d9617a711f3131c0de5d03808
Frnklyn <rortizjhonfrank@uss.edu.pe>
2026-07-16 15:10:14 -0500

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://evil-sides-train.loca.lt'; // URL Temporal de LocalTunnel
+const API_BASE_URL = 'https://spotty-cycles-press.loca.lt'; // URL Temporal de LocalTunnel
 
 const api = axios.create({
   baseURL: API_BASE_URL,
