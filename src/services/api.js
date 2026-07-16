fcecc314767d0d282855ec18e1854f9c77d38812
Frnklyn <rortizjhonfrank@uss.edu.pe>
2026-07-16 16:40:27 -0500

    feat: add api service module for authentication, prediction, and history management

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://thirty-lamps-check.loca.lt'; // URL Temporal de LocalTunnel
+const API_BASE_URL = 'https://better-bushes-raise.loca.lt'; // URL Temporal de LocalTunnel
 
 const api = axios.create({
   baseURL: API_BASE_URL,
